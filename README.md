02ce2db4dc37a0a621d74692031c6d3b3c960412
Lovertab <lovertabrown@gmail.com>
2024-04-09 21:54:53 -0400

    added readme file based on my prompts and added to write file and moved inquirer into init

---
+++ README.md
@@ -0,0 +1,32 @@
+# README File
+    ##Table of Contents
+    Description,Getting Started,Built With,Prerequisites,Installation,Usage,Tests,Contribution,License,Badges,Contact Me,Acknowledgments
+    // i want it numbered
+     ## Description
+    I wanted to create my own readme file so i can better understand how to use the syntax
+    to showcase my understanding of fs and using prompts
+    it gives me a template for creating a better readme
+    i learn that there are different ways to create a readme file
+    # Getting Started
+    ## Built With
+    NODE.JS
+    ##Prerequisites
+    npm install inquirer@^8 and npm install fs
+    ##Installation
+    clone or fork repo use npm init and npm instal
+    ##Usage
+    steps are:	1
+    ##Tests
+    no test atm
+    ##Contribution
+    myself
+    ## License
+    MIT License
+    ##Badges
+    n/a
+    ##Contact Me
+    lovertab
+    lovertabrown@gmail.com
+    ##Acknowledgments
+    tutoring
+    
--- index.js
+++ index.js
@@ -2,92 +2,149 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your project title?',
-            name:'project-title',
-        },
-        {
-            type: 'input',
-            message: 'What are your sections titles?',
-            name:'section-titles',
-        },
-        {
-            type: 'input',
-            message: 'What was your motivation?',
-            name:'motivation-description',
-        },
-        {
-            type: 'input',
-            message: 'Why did you build this project?',
-            name:'build-description',
-        },
-        {
-            type: 'input',
-            message: 'What problem does it solve?',
-            name:'problem-description',
-        },
-        {
-            type: 'input',
-            message: 'What did you learn?',
-            name:'learn-description',
-        },
-        {
-            type: 'checkbox',
-            message: 'Table of Content',
-            name: 'content',
-            choices: ['Installation', 'Usage', 'Credits', 'License','Badges', 'Tests', 'Questions'],
-        },
-        {
-            type: 'input',
-            message: 'What are the steps required to install your project?',
-            name: 'installation',
-        },
-        {
-            type: 'input',
-            message: 'Provide instructions and examples for usage. Include screenshots as needed.',
-            name: 'usage',
-        },
-        {
-            type: 'input',
-            message: 'List your collaborators, if any, with links to their GitHub profiles.',
-            name: 'credits',
-        },
-        {
-            type: 'checkbox',
-            message: 'What license are you using?',
-            name: 'licenses',
-            choices: ['MIT License', 'Apache License 2.0', 'GNU General Public License v3.0', 'BSD 2-Clause "Smplified" License', 'BSD 3-Clause "New" or "Revised" License'],
-        },
-        {
-            type: 'input',
-            message: 'Provide your badges here.',
-            name: 'badges',
-        },
-         {
-            type: 'input',
-            message: 'Provide examples of how to run your test here.',
-            name: 'tests',
-        },
-        {
-            type: 'input',
-            message: 'What questions do you have.',
-            // i need a way to add  my github username and my email here under questions with instructions on how to reach me for additional questions
-            name: 'questions',
-        },
-    ])
-    // this was given but idk what i'm supposed to add to it..
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'projectTitle',
+    },
+    {
+        type: 'checkbox',
+        message: 'Table of Content',
+        name: 'content',
+        choices: ['Description','Getting Started','Built With','Prerequisites','Installation', 'Usage','Tests','Contribution', 'License', 'Badges',  'Contact Me', 'Acknowledgments'],
+    },
+    {
+        type: 'input',
+        message: 'What was your motivation?',
+        name: 'motivationDescription',
+    },
+    {
+        type: 'input',
+        message: 'Why did you build this project?',
+        name: 'buildDescription',
+    },
+    {
+        type: 'input',
+        message: 'What problem does it solve?',
+        name: 'problemDescription',
+    },
+    {
+        type: 'input',
+        message: 'What did you learn?',
+        name: 'learnDescription',
+    },
+     {
+        type: 'checkbox',
+        message: 'What is it built with?',
+        name: 'builtWith',
+        choices: ['NEXT.JS', 'REACT', 'ANGULAR', 'BOOTSTRAP', 'JQUERY', 'SQL', 'EXPRESS.JS','NODE.JS','HTML','CSS'],
+    },
+    {
+        type: 'input',
+        message: 'What are some prerequisites needed before starting (example: npm install colors or inquirer@^8.0.0)?',
+        name: 'prerequisites',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions and examples for usage. Include screenshots as needed.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Provide examples of how to run your test here.',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.',
+        name: 'credit',
+    },
+    {
+        type: 'list',
+        message: 'What license are you using?',
+        name: 'license',
+        choices: ['MIT License', 'Apache License 2.0', 'GNU General Public License v3.0', 'BSD 2-Clause "Smplified" License', 'BSD 3-Clause "New" or "Revised" License'],
+    },
+    {
+        type: 'input',
+        message: 'Provide your badges here.',
+        name: 'badges',
+    },
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What resources did you use that was helpful when creating your application?',
+        name: 'acknowledgments',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile("README.md", data, (eer)) {
-    const READMETemplate = ``;
+function writeToFile(answers) {
+    const READMETemplate = `
+    # ${answers.projectTitle}
+    ##Table of Contents
+    ${answers.content}
+    // i want it numbered
+     ## Description
+    ${answers.motivationDescription}
+    ${answers.buildDescription}
+    ${answers.problemDescription}
+    ${answers.learnDescription}
+    # Getting Started
+    ## Built With
+    ${answers.builtWith}
+    ##Prerequisites
+    ${answers.prerequisites}
+    ##Installation
+    ${answers.installation}
+    ##Usage
+    ${answers.usage}
+    ##Tests
+    ${answers.test}
+    ##Contribution
+    ${answers.credit}
+    ## License
+    ${answers.license}
+    ##Badges
+    ${answers.badges}
+    ##Contact Me
+    ${answers.username}
+    ${answers.email}
+    ##Acknowledgments
+    ${answers.acknowledgments}
+    `;
+    fs.writeFile("README.md", READMETemplate, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('Sucess!!');
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+            writeToFile(answers);
+        });
+}
 
 // Function call to initialize app
 init();
